3642bba52f511f557b698245c8fc47d46c1dd184
Yaroslav <krupkin_yaroslav@ukr.net>
2022-01-14 16:35:16 +0200

    added_task_18

--- ThreeJS/task_17_particles/src/script.js
+++ ThreeJS/task_17_particles/src/script.js
@@ -19,21 +19,70 @@ const scene = new THREE.Scene()
  * Textures
  */
 const textureLoader = new THREE.TextureLoader()
+const particleTexture = textureLoader.load('/textures/particles/2.png')
 
 
-const particleGeometry = new THREE.SphereBufferGeometry(1.2,32,32)
+// const particleGeometry = new THREE.SphereBufferGeometry(1.2,32,32)
+// const particleMaterial = new THREE.PointsMaterial()
+// particleMaterial.size = 0.02
+// particleMaterial.sizeAttenuation = true
+
+// const particles = new THREE.Points(particleGeometry, particleMaterial)
+
+// const particleGeometryE = new THREE.SphereBufferGeometry(1,32,32)
+
+
+// const particlesE = new THREE.Points(particleGeometryE, particleMaterial)
+
+// scene.add(particles, particlesE)
+
+
+const particlesGeometry = new THREE.BufferGeometry()
+const count = 5000
+
+const positions = new Float32Array(count* 3)
+const colors = new Float32Array(count* 3)
+
+
+
+for(let i = 0; i < count * 3; i++){
+    positions[i] = (Math.random() - 0.5) * 10
+    colors[i] = Math.random()
+}
+
+particlesGeometry.setAttribute('position', new THREE.BufferAttribute(positions, 3))
+particlesGeometry.setAttribute('color', new THREE.BufferAttribute(colors, 3))
+
 const particleMaterial = new THREE.PointsMaterial()
-particleMaterial.size = 0.02
+particleMaterial.size = 0.2
 particleMaterial.sizeAttenuation = true
+// particleMaterial.color = new THREE.Color('#ff88cc')
+particleMaterial.transparent = true
+particleMaterial.alphaMap = particleTexture
+// particleMaterial.alphaTest = 0.001
+// particleMaterial.depthTest = false
+particleMaterial.depthWrite = false
+particleMaterial.blending = THREE.AdditiveBlending
 
-const particles = new THREE.Points(particleGeometry, particleMaterial)
 
-const particleGeometryE = new THREE.SphereBufferGeometry(1,32,32)
+particleMaterial.vertexColors =true
 
 
-const particlesE = new THREE.Points(particleGeometryE, particleMaterial)
+const particles = new THREE.Points(particlesGeometry, particleMaterial)
 
-scene.add(particles, particlesE)
+const newCoub = new THREE.Mesh(
+    new THREE.BoxBufferGeometry(),
+    new THREE.MeshBasicMaterial()
+)
+
+
+scene.add(particles, newCoub)
+
+
+
+
+
+
 /**
  * Sizes
  */
@@ -83,10 +132,23 @@ renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
  */
 const clock = new THREE.Clock()
 
+     console.log(particleMaterial.color)
+
+
 const tick = () =>
 {
     const elapsedTime = clock.getElapsedTime()
 
+    for(let i = 0; i < count; i++){
+
+        const i3 = i * 3
+        const x = particlesGeometry.attributes.position.array[i3]
+        particlesGeometry.attributes.position.array[i3 + 1] = Math.sin(elapsedTime + x)
+
+    }
+
+    particlesGeometry.attributes.position.needsUpdate = true
+        
     // Update controls
     controls.update()
 
