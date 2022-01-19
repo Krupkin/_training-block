c94e1dcc5d112e1e0a80a5902b816715af23b7fb
Yaroslav <krupkin_yaroslav@ukr.net>
2022-01-19 15:16:25 +0200

    added_task_20

--- ThreeJS/task_19_raycaster/src/script.js
+++ ThreeJS/task_19_raycaster/src/script.js
@@ -2,6 +2,7 @@ import './style.css'
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 import * as dat from 'dat.gui'
+import { Raycaster } from 'three'
 
 /**
  * Base
@@ -37,6 +38,9 @@ object3.position.x = 2
 
 scene.add(object1, object2, object3)
 
+
+const rayCaster = new THREE.Raycaster()
+
 /**
  * Sizes
  */
@@ -58,6 +62,31 @@ window.addEventListener('resize', () =>
     // Update renderer
     renderer.setSize(sizes.width, sizes.height)
     renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+})
+
+const mouse = new THREE.Vector2()
+
+window.addEventListener('mousemove', (evt) => {
+    mouse.x = evt.clientX / sizes.width * 2 -1 
+    mouse.y = - (evt.clientY / sizes.height) * 2 + 1
+
+})
+
+
+window.addEventListener('click', (evt) => {
+    if(currentInterSrct){
+        switch(currentInterSrct.object){
+            case object1:
+                console.log('object1')
+                break
+            case object2:
+                console.log('object2')
+                break
+            case object3:
+                console.log('object3')
+                break
+        }
+    }
 })
 
 /**
@@ -86,9 +115,53 @@ renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
  */
 const clock = new THREE.Clock()
 
+let currentInterSrct = null
+
 const tick = () =>
 {
     const elapsedTime = clock.getElapsedTime()
+
+    object1.position.y = Math.sin(elapsedTime * 0.4) * 1.5
+    object2.position.y = Math.sin(elapsedTime * 0.8) * 1.5
+    object3.position.y = Math.sin(elapsedTime * 1.5) * 1.5
+
+    rayCaster.setFromCamera(mouse, camera)
+
+    // const rayOrgin = new THREE.Vector3(-3, 0, 0)
+    // const rayDirection = new THREE.Vector3(10, 0, 0)
+
+    // rayDirection.normalize()
+
+    // rayCaster.set(rayOrgin, rayDirection)
+
+    const objectsToTest = [object1, object2, object3]
+    const interSects = rayCaster.intersectObjects(objectsToTest)
+
+    // for(const val of objectsToTest){
+    //     val.material.color.set('#00ffff')
+        
+    // }
+
+    // for(const val of interSects){
+    //     val.object.material.color.set('#0000ff')
+
+    // }
+
+
+    if(interSects.length){
+        if(currentInterSrct === null){
+            console.log('mouse enter')
+        }
+
+        currentInterSrct = interSects[0]
+
+    } 
+    else{
+        if(currentInterSrct){
+            console.log('mouse leave')
+        }
+        currentInterSrct = null
+    }
 
     // Update controls
     controls.update()
